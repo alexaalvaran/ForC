0389b71daaa815ba7979614e0b7c426a97f006f0
alexaalvaran <130896929+alexaalvaran@users.noreply.github.com>
2024-09-12 23:33:05 +1200

    create home page

---
+++ frontend/src/app/photos/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Photos() {
+    return (
+       <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1> our photos </h1>
+
+
+       </main>
+    );
+}
